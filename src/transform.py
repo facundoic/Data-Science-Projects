a9af7546cdcbbef3c9fcad5ea8ffce1f16d1d059
facundoic <facundoceballos1994@hotmail.com>
2021-07-01 15:46:55 -0300

    process the data and graph it

--- src/transform.py
+++ src/transform.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
-data_path = '/home/facundoic/Desktop/GitHub/Repositories/Data-Science-Projects/data/2005.csv'
+data_path = '/home/facundoic/Desktop/GitHub/Repositories/Data-Science-Projects/data/2018.csv'
 df = pd.read_csv(data_path,sep=';',encoding='latin-1')
 
 
@@ -18,5 +19,13 @@ print(df.duplicated(subset=['Grupo']).sum())
 print('------------------------------------------------------------------------')
 df.columns = df.columns.str.replace(' ','_')
 df = df.rename(columns={'Grupo_de_Causas_de_Defunción':'Causas'})
+df.sort_values(by=['Total'],ascending=False,inplace=True)
 
-print(df.head())
+df['Causas'] = [causa.split('.')[1] if '.' in causa else causa for causa in df['Causas'] ]
+total_label = [str(num) for num in df.iloc[2:7,4]]
+print(df.head(8))
+
+plt.title('Top 5 Causas de muerte en 2018')
+labels = df.iloc[2:7,2] + " : " + total_label
+plt.pie(df.iloc[2:7,4],labels=labels)
+plt.show()
